469369f7a6c5d5fcae1295647f4e21a50a1e20f2
bdabve <bdabve@gmail.com>
2023-07-02 16:14:35 +0100

    change city_longlatcmd.py

--- city_longLatCmd.py
+++ city_longLatCmd.py
@@ -3,14 +3,29 @@
 #
 # DESC    : find the longitude and latitude for a given city name
 # ----------------------------------------------------------------
+import os
+import dotenv
+import click
 import requests
 
+dotenv.load_dotenv()
+API_KEY = os.getenv('MAPQUESTAPI')
 
+
+@click.command()
+@click.argument('location')
 def find_city_info(location):
-    location = location
-    api_key = '0VWDo79OAhkZjFc4iItl0UfCSYdo6QbZ'
+    """
+    Retrieve and display information about a city.
+
+    Args:
+        location (str): The location (city) to search for.
+
+    Returns:
+        None
+    """
     url = 'http://www.mapquestapi.com/geocoding/v1/address'
-    params = {'key': api_key, 'location': location}
+    params = {'key': API_KEY, 'location': location}
 
     try:
         res = requests.get(url, params=params)
@@ -29,13 +44,4 @@ def find_city_info(location):
 
 
 if __name__ == '__main__':
-    from sys import argv
-    if len(argv[1:]) == 1:
-        location = argv[1]
-    else:
-        location = input('\n(?) Enter Locaion: ')
-
-    print()
-    print('(*) Information about: {}\n'.format(location))
-    find_city_info(location)
-    print()
+    find_city_info()
